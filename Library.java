a36d94b2135eafa4f9ae20969d1130777680a5b2
Marc-Velay <marc.velay@gmail.com>
2016-11-22 17:55:20 +0100

    added buttons n dummy pages

--- Library.java
+++ Library.java
@@ -7,11 +7,20 @@ import java.io.*;
 public class Library implements ActionListener {
 
     String name;
+    
     JTextArea testInput;
-    JButton addBookBtn;
+    JButton addDocBtn;
+    JButton checkoutDocBtn;
+    JButton createUser;
+    JButton login;
+
     JButton saveBook;
+
+    JButton checkout;
+    
     JFrame frame;
     JFrame newDocframe;
+    JFrame checkoutFrame;
 
     public Library(String name) {
         this.name = name;
@@ -26,13 +35,32 @@ public class Library implements ActionListener {
         JScrollPane scrollPane = new JScrollPane(testInput); 
         testInput.setEditable(true);
 
-        addBookBtn = new JButton("Add a new document");
-        addBookBtn.setMinimumSize(new Dimension(60, 50));
-        addBookBtn.setActionCommand("addDoc");
-        addBookBtn.addActionListener(this);
+        addDocBtn = new JButton("Add a new document");
+        addDocBtn.setMinimumSize(new Dimension(60, 50));
+        addDocBtn.setActionCommand("addDoc");
+        addDocBtn.addActionListener(this);
+
+        checkoutDocBtn = new JButton("Checkout a document");
+        checkoutDocBtn.setMinimumSize(new Dimension(60, 50));
+        checkoutDocBtn.setActionCommand("chkoutDoc");
+        checkoutDocBtn.addActionListener(this);
+
+        createUser = new JButton("Create a new user");
+        createUser.setMinimumSize(new Dimension(60, 50));
+        createUser.setActionCommand("createUser");
+        createUser.addActionListener(this);
+
+        login = new JButton("login");
+        login.setMinimumSize(new Dimension(60, 50));
+        login.setActionCommand("login");
+        login.addActionListener(this);
 
         toolBar.add(testInput);
-        toolBar.add(addBookBtn);
+        toolBar.add(addDocBtn);
+        toolBar.add(checkoutDocBtn);
+        toolBar.add(createUser);
+        toolBar.add(login);
+
         frame.add(toolBar);
         System.out.println("created lib");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -47,11 +75,34 @@ public class Library implements ActionListener {
                         frame.setVisible(false);
                         createNewDoc();
                         //Document newDoc = new Document(frame);
+                break; 
+                
+            case "chkoutDoc":                            
+                        frame.setVisible(false);
+                        checkoutDoc();
+                        //Document newDoc = new Document(frame);
+                break;
+
+            case "createUser":                            
+                        frame.setVisible(false);
+                        createUser();
+                        //Document newDoc = new Document(frame);
+                break;
+
+            case "login":                            
+                        frame.setVisible(false);
+                        login();
+                        //Document newDoc = new Document(frame);
                 break;
 
             case "saveDoc":
                         newDocframe.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-                        System.out.println("refreshed page");
+                        System.out.println("created doc");
+                break;
+
+            case "checkout":
+                        checkoutFrame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+                        System.out.println("checked out doc");
                 break;
         } 
     }
@@ -85,8 +136,41 @@ public class Library implements ActionListener {
         }});
     }
 
-    public void windowClosing(WindowEvent e){
-        System.out.println("closing");
+    private void checkoutDoc() {
+        checkoutFrame = new JFrame("Document Creation");
+        checkoutFrame.setMinimumSize(new Dimension(640,480));
+        checkoutFrame.setLayout(new BorderLayout());
+
+        JPanel getDocInfo = new JPanel();
+        getDocInfo.setMinimumSize(new Dimension(640, 480));
+
+        checkout = new JButton("checkout document");
+        checkout.setMinimumSize(new Dimension(60, 50));
+        checkout.setActionCommand("checkout");
+        checkout.addActionListener(this);
+
+        getDocInfo.add(checkout);
+
+        checkoutFrame.add(getDocInfo);
+        checkoutFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        checkoutFrame.pack();
+        checkoutFrame.setVisible(true);
+
+        checkoutFrame.addWindowListener( new WindowAdapter() {
+            public void windowClosing(WindowEvent e)
+            {
+                frame.setVisible(true);
+                frame.repaint();
+                checkoutFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        }});
+    }
+
+    public void createUser() {
+
+    }
+
+    public void login() {
+        
     }
 
     public static void main(String[] args){
